465c6518038f62a257b97e538fd1c2b36f9ab7dc
Jordan Pittman <jordan@cryptica.me>
2017-02-11 16:27:45 -0500

    Cleanup layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Birthday Problem</div>
+    <div class="panel panel-default">
+        <div class="panel-heading">Birthday Problem</div>
 
-                <div class="panel-body">
-                    <div class="form-group">
-                        <label for="" class="control-label">Total People</label>
-                        <input type="text" class="form-control" v-model="totalPeople">
-                    </div>
-                    <div class="form-group">
-                        <label for="" class="control-label">Number of Days</label>
-                        <input type="text" class="form-control" v-model="totalDays">
-                    </div>
-
-                    <span class="result">@{{ probability }}</span>
-                </div>
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="" class="control-label">Total People</label>
+                <input type="text" class="form-control" v-model="totalPeople">
             </div>
+            <div class="form-group">
+                <label for="" class="control-label">Number of Days</label>
+                <input type="text" class="form-control" v-model="totalDays">
+            </div>
+
+            <span class="result">@{{ probability }}</span>
         </div>
     </div>
-</div>
 @endsection
